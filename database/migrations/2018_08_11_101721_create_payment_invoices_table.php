1e829f8fe10545ed178d5e87bbb43cd4a294dcf2
Reynold <reynold_chong@hotmail.com>
2018-08-11 21:58:05 +0800

    payment invoice migration

---
+++ database/migrations/2018_08_11_101721_create_payment_invoices_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentOverviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_invoices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('invoice_no');
+            $table->string('payment_id');
+            $table->float('total', 15, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_invoices');
+    }
+}
