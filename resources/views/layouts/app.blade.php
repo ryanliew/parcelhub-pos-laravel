166bb755af108c818e6208be373ff87f70a3f2a8
Ryan Liew <fatal-fates@hotmail.com>
2019-01-15 22:33:47 +0800

    Bump version

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 
     <!-- Styles -->
     @yield('styles')
-    <link href="{{ asset('css/app.css?v=1.4') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css?v=1.5') }}" rel="stylesheet">
 
 </head>
 <body>
@@ -87,7 +87,7 @@
     </div>
 
     <script src="{{ asset('js/form.js') }}"></script>
-    <script src="{{ asset('js/app.js?v=2.26') }}"></script>
+    <script src="{{ asset('js/app.js?v=2.27') }}"></script>
     @yield('js')
 </body>
 </html>
