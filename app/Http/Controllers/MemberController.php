ae7448d74312ad284b7b2a7e490f022609dc1187
Ryan Liew <fatal-fates@hotmail.com>
2019-08-06 02:15:24 +0800

    Update MemberController.php

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -54,12 +54,14 @@ class MemberController extends Controller
 
         request()->validate([
             "name" => "required",
-            "phone_number" => ["required", $phone_unique],
+            "phone_number" => ["required", $phone_unique, 'regex:/^(\+?6?01?)[0-46-9]-*[0-9]{7,8}$/'],
             "email" => ["nullable", $mail_unique],
             "gender" => "required",
             "birthdate" => "required",
             "city" => "required",
             "state" => "required",
+        ], [
+            'phone_number.regex' => "Incorrect format. Please follow this format: +6012-1234567"
         ]);
     }
 
