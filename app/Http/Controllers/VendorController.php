9003cff6db61311c2286accb01c8b759a8bd9102
Ryan Liew <fatal-fates@hotmail.com>
2020-09-06 17:38:13 +0800

    Update VendorController.php

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -60,6 +60,6 @@ class VendorController extends Controller
 
     public function list()
     {
-        return Vendor::whereNotIn('id', [5,9,14,12,18])->get();
+        return Vendor::whereNotIn('id', [5,9,14,12,18,25])->get();
     }
 }
